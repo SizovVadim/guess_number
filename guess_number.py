cfac10f1f591b292466d90ac6c7bd53fba5a6b71
Vadim Sizov <vadim.sizovpgk@gmail.com>
2025-01-26 21:02:36 +0300

    guess_number_ver_2.0

--- guess_number.py
+++ guess_number.py
@@ -2,12 +2,13 @@ from random import randint
 
 number_leader = randint(1, 100)
 number_player = int(input('Введите число: '))
-while number_leader != number_player:
+while True:
     if number_leader > number_player:
         print('Ваше число меньше того, что загадано')
         number_player = int(input('Попробуй еще раз!'))
-    elif number_leader < number_player:
+    if number_leader < number_player:
         print('Ваше число больше того, что загадано')
         number_player = int(input('Попробуй еще раз!'))
-if number_player == number_leader:
-    print('Отличная интуиция! Вы угадали число :)')
+    if number_player == number_leader:
+        break
+print('Отличная интуиция! Вы угадали число :)')
